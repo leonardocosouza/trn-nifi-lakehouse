273906def085cc434596598aa53f95c8f5581277
Leonardo Côco <leonardocosouza@gmail.com>
2024-10-16 20:51:25 -0300

    ini

--- docker-compose/scripts/entrypoint.sh
+++ docker-compose/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-export HADOOP_HOME=/opt/hadoop-3.3.6
+export HADOOP_HOME=/opt/hadoop-3.2.0
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
 export JAVA_HOME=/usr/local/openjdk-8
 export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}
